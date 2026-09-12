770001e3b19e6b395c8dcf4ef764091bbb22552c
cedanosergio11 <cedanosergio11@gmail.com>
2026-09-11 22:19:42 -0700

    Add src/theme.js

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+export const COLORS = {
+  black: '#000000',
+  steel: '#353d3e',
+  gold: '#e9c31e',
+  white: '#ffffff',
+  silver: '#bfbfc7',
+  gray: '#6f7273',
+  softGold: '#fbf6dc',
+  on: '#4a8f52',
+  off: '#c45c5c',
+  insuff: '#bfbfc7',
+}
+
+export const PIE_COLORS = [
+  '#353d3e',
+  '#e9c31e',
+  '#6f7273',
+  '#bfbfc7',
+  '#8a7a2a',
+  '#5a6465',
+  '#c4a91a',
+  '#9a9aa3',
+]
